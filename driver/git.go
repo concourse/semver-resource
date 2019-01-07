d03af6e52de0374023f1cc436411c6e125f5669d
jduv <jduv@7factor.io>
2019-01-06 22:00:55 -0500

    Adding debug around the error from git passphrase

--- driver/git.go
+++ driver/git.go
@@ -225,6 +225,8 @@ func isPrivateKeyEncrypted(path string) bool {
 	cmd := exec.Command(`ssh-keygen`, `-y`, `-f`, path, `-P`, passphrase)
 	err := cmd.Run()
 
+	println("Error attempting to access private key: %v", err.Error())
+
 	return err != nil
 }
 
